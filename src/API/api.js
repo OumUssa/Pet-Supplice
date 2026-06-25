235baac3c4dd672f67891c483f056e8bbd9126e9
Ussa <brostk962@gmail.com>
2026-06-26 00:28:34 +0700

    update

--- src/API/api.js
+++ src/API/api.js
@@ -1,5 +1,5 @@
-// const API_BASE_URL = "https://petsupplice.cms-jubpet.linkpc.net/api";
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = "https://petsupplice.cms-jubpet.linkpc.net/api";
+// const API_BASE_URL = "http://127.0.0.1:8000/api";
 
 export const registerUser = async (userData) => {
   try {
